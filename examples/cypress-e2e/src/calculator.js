44f2fa1cc722737069af77b6ae2b99ff68196a2e
Pavel Sturc <psturc@redhat.com>
2025-09-25 14:10:02 +0200

    Test calculator change

--- examples/cypress-e2e/src/calculator.js
+++ examples/cypress-e2e/src/calculator.js
@@ -44,7 +44,7 @@ class Calculator {
             result = num1 + num2;
             operationSymbol = '+';
             console.log('Addition performed:', result);
-            console.log('Webpack-based coverage TIA test');
+            console.log('Testing precision with webpack instrumentation');
                 break;
             case 'subtract':
                 result = num1 - num2;
