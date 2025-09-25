f9c219c599300953e08b220bcc02be94016fb55f
Pavel Sturc <psturc@redhat.com>
2025-09-25 15:50:02 +0200

    Add utils.js with utility functions

---
+++ examples/cypress-e2e/src/utils.js
@@ -0,0 +1,16 @@
+/**
+ * Utility functions for the application
+ */
+
+export function formatNumber(num) {
+  // Updated formatting function for TIA test
+  return parseFloat(num).toFixed(2);
+}
+
+export function isValidNumber(value) {
+  return !isNaN(value) && isFinite(value);
+}
+
+export function sanitizeInput(input) {
+  return input.toString().trim();
+}
